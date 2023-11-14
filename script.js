f763153c4acf8fb11c8db6ddb1b0f17267bdff25
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2023-11-13 20:23:23 -0600

    Testing Trueway Geocoding Reverse Geocoding API

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const p = document.querySelector("#test");
 function position_success(position){
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
-  p.innerHTML = `Your position is ${lat},${long}`;
+  getLocation(lat,long);
 }
 
 function position_error(){
@@ -16,4 +16,27 @@ if(!navigator.geolocation) {
 }
 else{
   navigator.geolocation.getCurrentPosition(position_success, position_error);
+}
+
+async function getLocation(lat,long){
+    const url = `https://trueway-geocoding.p.rapidapi.com/ReverseGeocode?location=${lat}%2C${long}&language=en`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '72bef8a0e0mshc2a48bfbf76a33cp1f0bf9jsn2eba487d9cba',
+        'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      printLocation(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+function printLocation(json){
+  p.innerHTML = `${json.results[0].locality}, ${json.results[0].region}`
 }
