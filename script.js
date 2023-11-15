3acf80c9f5534977384539fc98d94d39df4eb96a
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2023-11-14 23:05:07 -0600

    Started outlining the look of the website

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Selecting the paragraph element with id test
-const p = document.querySelector("#test");
+/*const p = document.querySelector("#test");
 
 //Handling the getCurrentPosition function
 function position_success(position){
@@ -21,7 +21,7 @@ else{
 }
 
 //Utilizing the Trueway Reverse Geocoding API
-/*async function getLocation(lat,long){
+async function getLocation(lat,long){
     const url = `https://trueway-geocoding.p.rapidapi.com/ReverseGeocode?location=${lat}%2C${long}&language=en`;
     const options = {
       method: 'GET',
@@ -42,7 +42,7 @@ else{
 //Function to print results retured by the Reverse Geocoding
 function printLocation(json){
   p.textContent += `${json.results[0].locality}, ${json.results[0].region}`
-}*/
+}
 
 async function getWeather(lat,long){
   const url = `https://world-weather-online-api1.p.rapidapi.com/weather.ashx?q=${lat}%2C${long}&num_of_days=5&lang=en&aqi=yes&alerts=no&format=json`;
@@ -65,4 +65,4 @@ async function getWeather(lat,long){
 
 function printWeather(json){
   p.textContent += `${json.data.current_condition[0].temp_F}`
-}
+}*/
