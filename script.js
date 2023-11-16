ed03ae69c89a52d96909c5821293b5533d74433f
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2023-11-15 21:13:23 -0600

    Added other weather elements and allow hooked up majority of the info from Weather Api

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-//Selecting the paragraph element with id test
-/*const p = document.querySelector("#test");
-
 //Handling the getCurrentPosition function
 function position_success(position){
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
-  //getLocation(lat,long);
+  getLocation(lat,long);
   getWeather(lat,long);
 }
 
@@ -34,14 +31,15 @@ async function getLocation(lat,long){
     try {
       const response = await fetch(url, options);
       const result = await response.json();
-      printLocation(result);
+      insertLocation(result);
     } catch (error) {
       console.error(error);
     }
   }
 //Function to print results retured by the Reverse Geocoding
-function printLocation(json){
-  p.textContent += `${json.results[0].locality}, ${json.results[0].region}`
+function insertLocation(json){
+  const curr_city = document.querySelector('#city');
+  curr_city.textContent = `${json.results[0].locality}, ${json.results[0].region}`;
 }
 
 async function getWeather(lat,long){
@@ -57,12 +55,63 @@ async function getWeather(lat,long){
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    printWeather(result);
+    insertTemp(result);
+    insertFeelsLike(result);
+    insertWindSpeed(result);
+    insertPrecip(result);
+    insertHumidity(result);
+    insertUV(result);
   } catch (error) {
     console.error(error);
   }
 }
 
-function printWeather(json){
-  p.textContent += `${json.data.current_condition[0].temp_F}`
-}*/
+function insertTemp(json){
+  const curr_temp = document.querySelector('#temp');
+  curr_temp.textContent = `${json.data.current_condition[0].temp_F}°F`;
+}
+
+function insertFeelsLike(json){
+  const feel_like = document.querySelector('#feels-info');
+  feel_like.textContent = `${json.data.current_condition[0].FeelsLikeF}°F`;
+}
+
+function insertWindSpeed(json){
+  const wind_speed = document.querySelector('#speed-info');
+  const wind_direction = document.querySelector('#speed-dir');
+
+  wind_speed.textContent = `${json.data.current_condition[0].windspeedMiles} mph`;
+  wind_direction.textContent = `${json.data.current_condition[0].winddir16Point}`;
+}
+
+function insertPrecip(json){
+  const precipitation = document.querySelector('#precip-info');
+  precipitation.textContent = `${json.data.current_condition[0].precipMM} mm`;
+}
+
+function insertHumidity(json){
+  const humidity = document.querySelector('#humid-info');
+  humidity.textContent = `${json.data.current_condition[0].humidity}%`
+}
+
+function insertUV(json){
+  const uv = document.querySelector('#uv-info');
+  const uv_scale = document.querySelector('#uv-scale');
+
+  uv.textContent = `${json.data.current_condition[0].uvIndex}`
+  if(Number(uv.textContent) <= 2){
+    uv_scale.textContent = 'Low';
+  }
+  else if(Number(uv.textContent) <= 5){
+    uv_scale.textContent = 'Moderate';
+  }
+  else if(Number(uv.textContent) <= 7){
+    uv_scale.textContent = 'High';
+  }
+  else if(Number(uv.textContent) <= 10){
+    uv_scale.textContent = 'Very high';
+  }
+  else{
+    uv_scale.textContent = 'Extreme';
+  }
+}
